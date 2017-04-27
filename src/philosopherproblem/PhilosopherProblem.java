8dfe944ac9f1a798493269c70a334f81b53671cc
Vince Seely <Vince Seely>
2017-04-27 00:31:32 -0500

    Create a list of philosophers and a list of forks Running all of the Philosopher threads

--- src/philosopherproblem/PhilosopherProblem.java
+++ src/philosopherproblem/PhilosopherProblem.java
@@ -5,6 +5,10 @@
  */
 package philosopherproblem;
 
+import java.util.Scanner;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
  *
  * @author vince
@@ -14,16 +18,33 @@ public class PhilosopherProblem {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
-        // TODO code application logic here
-//         Thread myThreads[] = new Thread[argsize];
-//         for (int j = 0; j < argSize; j++) {
-//             myThreads[j] = new Thread(new wThreads(j));
-//             myThreads[j].start();
-//         }
-//         for (int j = 0; j < argSize; j++) {
-//             myThreads[j].join(); //todo add catch exception
-//         } 
+   public static void main(String[] args) 
+   {
+   // TODO code application logic here
+      Scanner input = new Scanner(System.in);
+      System.out.println("How many Philosophers do we have today?");
+      int totalPhilosophers = input.nextInt();
+      input.nextLine();
+      Philosopher[] myPhilosophers = new Philosopher[totalPhilosophers];
+      Fork[] philosFork = new Fork[totalPhilosophers];
+      for (int i = 0; i < totalPhilosophers; i++)
+      {
+         philosFork[i] = new Fork();
+      }
+      for (int i = 0; i < totalPhilosophers; i++)
+      {
+         //TODO: give them their forks
+         myPhilosophers[i] = new Philosopher();
+         myPhilosophers[i].start();
+      }
+      for(int i = 0; i < totalPhilosophers; i++)
+      {
+         try {
+            myPhilosophers[i].join();
+         } catch (InterruptedException ex) {
+            Logger.getLogger(PhilosopherProblem.class.getName()).log(Level.SEVERE, null, ex);
+         }
+      }
    }
     
 }
